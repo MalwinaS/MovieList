4d59bcc392abf64d2a831854726c7fc6c7bad8f8
Malwina S <malwinastepien93@gmail.com>
2023-11-05 13:51:56 +0100

    add MovieCard

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>Title {{ title }}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p>Description {{ description }}</p>
+    <nav>
+      <a :href=link>Link</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default { 
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
